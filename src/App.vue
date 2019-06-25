d4bad5f25397aafde6cbb33d07b4d427861782e0
Audrey Magwili <audreymagwili@Audreys-MacBook-Air.local>
2019-06-24 17:21:05 -0700

    initial Vue app

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Shopping App"/>
+    <Showcase v-bind:photos="photos"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Showcase from './components/Showcase.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Showcase
+  },
+  data() {
+    return {
+      photos: null
+    }
+  },
+  mounted () {
+    fetch('https://jsonplaceholder.typicode.com/photos/?limit=10')
+      .then(response => response.json())
+      .then(json => (this.photos = json));
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Showcase.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div class="showcase">
+      <h2>showcase</h2>
+      <div class="showcase-tile"
+        v-for="photo in photos"
+        v-bind="photo">
+        <img :src="photo.thumnailUrl" alt="photo thumbnail">
+        <p>{{photo.title}}</p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Showcase',
+  props: ["photos"]
+}
+</script>
+
+<style lang="css" scoped>
+</style>
